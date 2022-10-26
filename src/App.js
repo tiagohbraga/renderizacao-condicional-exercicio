5675a07efe12fd772a4729f43c3777203fa3b1ba
Tiago Braga <tiagohbraga@gmail.com>
2022-10-25 21:22:31 -0300

    mudando de pagina apertando botoes

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import styled, { createGlobalStyle } from "styled-components";
+import TelaLogin from "./components/TelaLogin/TelaLogin";
+import TelaCadastro from "./components/TelaCadastro/TelaCadastro";
+import TelaUsuarioCadastrado from "./components/TelaUsuarioCadastrado/TelaUsuarioCadastrado.js";
+import { useState } from "react";
+import { TelaCadastroEndereço } from "./components/TelaCadastroEndereco/TelaCadastroEndereco";
+
+const GlobalStyled = createGlobalStyle`
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+`
+const MainContainer = styled.main`
+  height: 100vh;
+`
+
+function App() {
+  const [valorCondicional, setValorCondicional] = useState(1)
+
+  const mudarTela = (valor) => {
+    setValorCondicional(valor)
+  }
+
+  // Resolução do exercício de fixação
+  const renderizaTela = () => {
+    switch (valorCondicional) {
+      case 1:
+        return <TelaLogin mudarTela={mudarTela} />;
+      case 2:
+        return <TelaCadastro mudarTela={mudarTela} />;
+      case 3:
+        return <TelaCadastroEndereço mudarTela={mudarTela}/>;
+      case 4:
+        return <TelaUsuarioCadastrado />;
+      default:
+        alert("Algo deu errado, recarregue a pagina")  
+    }
+  }
+
+  return (
+    <MainContainer >
+      <GlobalStyled />
+      {renderizaTela()}
+    </MainContainer>
+  );
+}
+
+export default App;
---
+++ src/components/TelaUsuarioCadastrado/TelaUsuarioCadastrado.js.js
@@ -0,0 +1,16 @@
+import { Div, Titulo } from "./styled";
+import TelaLogin from "../TelaLogin/TelaLogin";
+import { BackToLoginButton } from "../TelaCadastro/styled";
+
+function TelaUsuarioCadastrado (props){
+    return (
+        <Div>
+        
+            <Titulo>Você concluiu o cadastro, parabéns!!!</Titulo>
+                    
+        </Div>
+        
+    )
+}
+
+export default TelaUsuarioCadastrado;
